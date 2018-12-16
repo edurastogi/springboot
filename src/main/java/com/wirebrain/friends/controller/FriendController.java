a216b45b1067dff8a0d6cd127bafb249fc38bcea
aditya rastogi <edurastogi@gmail.com>
2018-12-15 22:12:52 -0500

    minor changes

--- src/main/java/com/wirebrain/friends/controller/FriendController.java
+++ src/main/java/com/wirebrain/friends/controller/FriendController.java
@@ -3,6 +3,8 @@ package com.wirebrain.friends.controller;
 import com.wirebrain.friends.model.Friend;
 import com.wirebrain.friends.service.FriendService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -24,8 +26,12 @@ public class FriendController {
     }
 
     @PutMapping("/friend")
-    Friend update(@RequestBody Friend friend) {
-        return friendService.save(friend);
+    ResponseEntity<Friend> update(@RequestBody Friend friend) {
+        if(friendService.findById(friend.getId()).isPresent()){
+            return new ResponseEntity(friendService.save(friend), HttpStatus.OK);
+        }else{
+            return new ResponseEntity(friend, HttpStatus.BAD_REQUEST);
+        }
     }
 
     @DeleteMapping("/friend/{id}")
